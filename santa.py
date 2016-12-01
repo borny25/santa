cad75955503b99e3da78ab3203a4cff802d5ac0f
Born <Adam.Born@springernature.com>
2016-12-01 11:15:19 +0000

    initial commit

---
+++ santa.py
@@ -0,0 +1,49 @@
+import random
+
+import itertools
+
+
+
+people = [
+
+    'Tom',
+
+    'Dick',
+
+    'Harry',
+
+    'Jane',
+
+    'Sally',
+
+]
+
+
+
+def valid(a, b):
+
+    for i, j in itertools.zip_longest(a, b):
+
+        if i == j: return False
+
+    return True
+
+
+
+a = list(people)
+
+b = list(people)
+
+
+
+while not valid(a, b):
+
+    random.shuffle(a)
+
+    random.shuffle(b)
+
+
+
+for i, j in itertools.zip_longest(a, b):
+
+    print('%s gives a gift to %s.' % (i, j))
